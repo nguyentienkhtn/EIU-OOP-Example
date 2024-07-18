a4001b719048427327c984afde147d2805680eed
nguyentienkhtn <tiennd@saigontech.edu.vn>
2024-07-18 11:01:57 +0700

    aaaaa

--- Chapter3/src/App.java
+++ Chapter3/src/App.java
@@ -2,7 +2,13 @@ public class App {
     public static void main(String[] args) 
     {
         Calc abc = (a,b) -> a*b; //lambda expression
-
+        
+        Calc def = new Calc() {
+            @Override
+            public int mult(int a, int b) {
+                return a*b;
+            }
+        };
         int result = abc.mult(10, 5);
         System.out.println(result);
 
