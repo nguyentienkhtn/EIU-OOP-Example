d26f1b2c06e8ff5f06b193d353cbe14c1ab8789b
nguyentienkhtn <tiennd@saigontech.edu.vn>
2024-07-18 10:44:47 +0700

    aaaa

--- Chapter3/src/App.java
+++ Chapter3/src/App.java
@@ -4,6 +4,16 @@ public class App {
         // due to polymorphism
         Submit s = new Solution(); 
         s.addYourSignature();
-        
+
+        Submit s2 = new Submit() {// annonymous inner class
+
+            @Override
+            public void addYourSignature() {
+                System.out.println("I'm so lazy, you are, too");
+            }
+            
+        };
+        s2.addYourSignature();
+
     }
 }
