04666de68abc1dc42fef21d143f21f96408aa366
nguyentienkhtn <tiennd@saigontech.edu.vn>
2024-07-18 10:12:35 +0700

    class

---
+++ Chapter3/src/SuperRichStudent.java
@@ -0,0 +1,14 @@
+public class SuperRichStudent extends Student{
+    
+    public SuperRichStudent(String id, String name, double score) {
+        super(id, name, score);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void diHoc() {
+        // TODO Auto-generated method stub
+        System.out.println("Going by rocket");
+    }
+
+}
