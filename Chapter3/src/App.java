ea623da065432ae88dbf1225f62e7b010bdb1cf1
nguyentienkhtn <tiennd@saigontech.edu.vn>
2024-07-18 11:09:34 +0700

    acbc

--- Chapter3/src/App.java
+++ Chapter3/src/App.java
@@ -1,16 +1,27 @@
 public class App {
     public static void main(String[] args) 
     {
+        Interface3 i = () -> 
+        {  
+            System.out.println("So so"); 
+            System.out.println("So so");
+        };
+        Calc2 calc2 = (a, b) -> {
+            int c = b*a;
+            System.out.println(c);
+        };
+        calc2.mult(10, 10);
+
         Calc abc = (a,b) -> a*b; //lambda expression
         
-        Calc def = new Calc() {
-            @Override
-            public int mult(int a, int b) {
-                return a*b;
-            }
-        };
-        int result = abc.mult(10, 5);
-        System.out.println(result);
+        // Calc def = new Calc() {
+        //     @Override
+        //     public int mult(int a, int b) {
+        //         return a*b;
+        //     }
+        // };
+        // int result = abc.mult(10, 5);
+        // System.out.println(result);
 
     }
 }
---
+++ Chapter3/src/Calc2.java
@@ -0,0 +1,3 @@
+public interface Calc2 {
+    void mult(int a, int b);
+}
