b7aa9f4ebf36048a3237c6ee422dd5a5657195ae
nguyentienkhtn <tiennd@saigontech.edu.vn>
2024-07-18 10:19:55 +0700

    interface

--- Chapter3/src/Student.java
+++ Chapter3/src/Student.java
@@ -1,16 +1,6 @@
-public abstract class Student {
-    private String id;
-    private String name;
-    private double score;
-    public Student(String id, String name, double score) {
-        this.id = id;
-        this.name = name;
-        this.score = score;
-    }
-    @Override
-    public String toString() {
-        return "Student [id=" + id + ", name=" + name + ", score=" + score + "]";
-    }
-    
+public interface Student {
+
     public abstract void diHoc(); //header
+    public abstract void diVe();
+
 }
--- Chapter3/src/SuperRichStudent.java
+++ Chapter3/src/SuperRichStudent.java
@@ -1,14 +1,17 @@
 public class SuperRichStudent extends Student{
     
-    public SuperRichStudent(String id, String name, double score) {
-        super(id, name, score);
-        //TODO Auto-generated constructor stub
-    }
+    
 
     @Override
     public void diHoc() {
         // TODO Auto-generated method stub
         System.out.println("Going by rocket");
+    }
+
+    @Override
+    public void diVe() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'diVe'");
     }
 
 }
