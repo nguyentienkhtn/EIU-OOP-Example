5225b7e8b07230d4e5e9a1e926ce051231710a80
nguyentienkhtn <tiennd@saigontech.edu.vn>
2024-07-25 15:03:50 +0700

    book

--- Chapter3/src/App.java
+++ Chapter3/src/App.java
@@ -1,18 +1,41 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
 public class App {
     public static void main(String[] args) 
     {
-        Interface3 i = () -> 
-        {  
-            System.out.println("So so"); 
-            System.out.println("So so");
-        };
-        Calc2 calc2 = (a, b) -> {
-            int c = b*a;
-            System.out.println(c);
-        };
-        calc2.mult(10, 10);
+        Deque<String> deque = new ArrayDeque<>();
 
-        Calc abc = (a,b) -> a*b; //lambda expression
+        // Add elements at the end
+        deque.addLast("Element 1");
+        deque.addLast("Element 2");
+        deque.addLast("Element 3");
+
+        // Add elements at the front
+        deque.addFirst("Element 0");
+
+        // Display the elements
+        System.out.println("Deque: " + deque);
+
+        // Remove elements from the front
+        System.out.println("Removed from front: " + deque.removeFirst());
+
+        // Remove elements from the end
+        System.out.println("Removed from end: " + deque.removeLast());
+
+        // Display the elements
+        System.out.println("Deque after removals: " + deque);
+        // Interface3 i = () -> 
+        // {  
+        //     System.out.println("So so"); 
+        //     System.out.println("So so");
+        // };
+        // Calc2 calc2 = (a, b) -> {
+        //     int c = b*a;
+        //     System.out.println(c);
+        // };
+        // calc2.mult(10, 10);
+
+        // Calc abc = (a,b) -> a*b; //lambda expression
         
         // Calc def = new Calc() {
         //     @Override
